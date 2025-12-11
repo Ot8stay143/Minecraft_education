ae779b7472c894001829c9eccc6aa059de4522b9
Ot8stay143 <simona12kov@gmail.com>
2025-12-11 17:05:12 +0100

    code change

--- pyramid.py
+++ pyramid.py
@@ -1,34 +1,17 @@
 agent.teleport_to_player()
-def build_pyramid(size: int):
-    # Put agent in safe place
-    agent.teleport_to_player()
-    agent.move(FORWARD, 3)
-    agent.move(UP, 1)
-
-    layer = 0
-
-    while size - 2 * layer > 0:
-        length = size - 2 * layer
-        build_square(length)
-
-        # go to next layer
-        agent.move(UP, 1)
-        agent.move(FORWARD, 1)
-        agent.move(LEFT, 1)
-
-        layer += 1
-
-
-def build_square(n: int):
-    # builds a hollow square
-    for side in range(4):
-        for step in range(n - 1):
-            agent.place(DOWN)
+def build_pyramid(base_side):
+    agent.set_slot(1)
+    if base_side % 2 != 1:
+        player.say("Length of base side must be odd")
+    else:
+        for side_length in range(base_side - 1, 0, -2):
+            agent.set_assist(PLACE_ON_MOVE, True)
+            for wall in range(4):
+                agent.move(FORWARD, side_length)
+                agent.turn_left()
+            agent.move(UP, 1)
+            agent.set_assist(PLACE_ON_MOVE, False)
             agent.move(FORWARD, 1)
-        agent.turn(RIGHT)
-
-
-def on_chat(size):
-    build_pyramid(size)
-
-player.on_chat("pyramid", on_chat)
+        agent.set_assist(PLACE_ON_MOVE, True)
+        agent.move(UP, 1)
+player.on_chat("build", build_pyramid)
